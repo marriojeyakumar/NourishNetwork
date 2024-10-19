c2816dbf16d5d786eb34bd4b9aa36969e1ce0507
Simon Valentino <simontvalentino@gmail.com>
2024-10-19 13:33:15 -0400

    Added business login start

--- app/src/main/java/com/example/nourishnet/BusinessLoginActivity.java
+++ app/src/main/java/com/example/nourishnet/BusinessLoginActivity.java
@@ -1,8 +1,6 @@
 package com.example.nourishnet;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,16 +8,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class BusinessLoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void businessLogin(View view) {
-        Intent intent = new Intent(this, BusinessLoginActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_business_login);
     }
 }
